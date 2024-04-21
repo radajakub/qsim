dc1db22be8283e864ad243c5bf885a344c1b0afc
Jakub Rada <dev.jakubrada@icloud.com>
2024-04-21 21:57:46 +0200

    Add simple print test

--- Makefile
+++ Makefile
@@ -7,18 +7,24 @@ BUILD_DIR=./build
 CXXFLAGS=-std=c++11 -O2 -march=native
 
 # Find all the C++ files in src and test directories
-SRCS=$(SRC_DIR)/main.cpp $(SRC_DIR)/complex.cpp $(SRC_DIR)/qubit.cpp
+LIBS=$(SRC_DIR)/complex.cpp $(SRC_DIR)/qubit.cpp
+
+SRCS=$(LIBS) $(SRC_DIR)/main.cpp
+TSTS=$(LIBS) $(SRC_DIR)/test.cpp
 
 # Create object file names by replacing .cpp with .o
 OBJS=$(patsubst $(SRC_DIR)/%.cpp,$(BUILD_DIR)/%.o,$(SRCS))
+TST_OBJS=$(patsubst $(SRC_DIR)/%.cpp,$(BUILD_DIR)/%.o,$(TSTS))
 
 # Targets
 TARGET=$(BUILD_DIR)/solve
+TST_TARGET=$(BUILD_DIR)/test
 
-# Build targets
-all: $(TARGET)
 
 $(TARGET): $(OBJS)
+	$(CXX) $(CXXFLAGS) $^ -o $@
+
+$(TST_TARGET): $(TST_OBJS)
 	$(CXX) $(CXXFLAGS) $^ -o $@
 
 # Compile source files
@@ -26,8 +32,14 @@ $(BUILD_DIR)/%.o: $(SRC_DIR)/%.cpp
 	@mkdir -p $(BUILD_DIR)
 	$(CXX) $(CXXFLAGS) -c $< -o $@
 
+# Build targets
+all: $(TARGET)
+
+test: $(TST_TARGET)
+	$(TST_TARGET)
+
 # Clean
 clean:
 	rm -rf $(BUILD_DIR)
 
-.PHONY: clean all
+.PHONY: clean all test
---
+++ src/test.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+#include "complex.hpp"
+#include "qubit.hpp"
+
+void test_complex() {
+    qs::Complex a(1, 2);
+    qs::Complex b(3, 4);
+    qs::Complex c = a + b;
+    std::cout << c << std::endl;
+    c = a - b;
+    std::cout << c << std::endl;
+    c = a * b;
+    std::cout << c << std::endl;
+    c += a;
+    std::cout << c << std::endl;
+    c = c.conjugate();
+    std::cout << c << std::endl;
+}
+
+void test_qubits() {
+    qs::Ket q0(qs::BasicQubits::ZERO);
+    q0.symbol();
+    std::cout << " = ";
+    q0.vector();
+    std::cout << std::endl;
+
+    qs::Ket q1(qs::BasicQubits::ONE);
+    q1.symbol();
+    std::cout << " = ";
+    q1.vector();
+    std::cout << std::endl;
+
+    qs::Ket q01 = q0.tensor(q1);
+    q01.symbol();
+    std::cout << " = ";
+    q01.vector();
+    std::cout << std::endl;
+
+    qs::Ket q10 = q1.tensor(q0);
+    q10.symbol();
+    std::cout << " = ";
+    q10.vector();
+    std::cout << std::endl;
+}
+
+int main(int argc, char* argv[]) {
+    std::cout << "Test Complex library" << std::endl;
+    test_complex();
+    std::cout << std::endl;
+
+    std::cout << "Test Qubits library" << std::endl;
+    test_qubits();
+    std::cout << std::endl;
+    return 0;
+}
