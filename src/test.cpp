c1fe26de11207634000f1d9ffe627ee9c58112ca
Jakub Rada <dev.jakubrada@icloud.com>
2024-04-22 17:26:55 +0200

    Extend and improve print tests

--- src/test.cpp
+++ src/test.cpp
@@ -4,94 +4,101 @@
 #include "qubit.hpp"
 #include "unitary.hpp"
 
+void print_complex_operation(qs::Complex &a, qs::Complex &b, qs::Complex &res, std::string str) {
+    std::cout << a.str() << " " << str << " " << b.str() << " = " << res.str() << std::endl;
+}
+
+void print_complex_operation(qs::Complex &a, qs::Complex &res, std::string str) {
+    std::cout << a.str() << " " << str << " = " << res.str() << std::endl;
+}
+
+void print_qubit(qs::Qubit &q) {
+    q.symbol();
+    std::cout << " = ";
+    q.vector();
+    std::cout << std::endl;
+}
+
+void print_unitary(qs::Unitary &u) {
+    u.symbol();
+    std::cout << " = " << std::endl;
+    u.matrix();
+}
+
 void test_complex() {
     qs::Complex a(1, 2);
     qs::Complex b(3, 4);
-    qs::Complex c = a + b;
-    std::cout << c.str() << std::endl;
-    c = a - b;
-    std::cout << c.str() << std::endl;
-    c = a * b;
-    std::cout << c.str() << std::endl;
-    c += a;
-    std::cout << c.str() << std::endl;
-    c = c.conjugate();
-    std::cout << c.str() << std::endl;
+    qs::Complex res;
+
+    res = a + b;
+    print_complex_operation(a, b, res, "+");
+
+    res = a - b;
+    print_complex_operation(a, b, res, "-");
+
+    res = a * b;
+    print_complex_operation(a, b, res, "*");
+
+    res = a.conjugate();
+    print_complex_operation(a, res, "conjugate");
 }
 
 void test_qubits() {
     qs::Ket q0(qs::BasicQubits::ZERO);
-    q0.symbol();
-    std::cout << " = ";
-    q0.vector();
-    std::cout << std::endl;
+    print_qubit(q0);
 
     qs::Ket q1(qs::BasicQubits::ONE);
-    q1.symbol();
-    std::cout << " = ";
-    q1.vector();
-    std::cout << std::endl;
+    print_qubit(q1);
 
     qs::Ket q_minus(qs::BasicQubits::MINUS);
-    q_minus.symbol();
-    std::cout << " = ";
-    q_minus.vector();
-    std::cout << std::endl;
+    print_qubit(q_minus);
 
     qs::Ket q_plus(qs::BasicQubits::PLUS);
-    q_plus.symbol();
-    std::cout << " = ";
-    q_plus.vector();
-    std::cout << std::endl;
+    print_qubit(q_plus);
 
-    qs::Ket q01 = q0.tensor(q1);
-    q01.symbol();
-    std::cout << " = ";
-    q01.vector();
-    std::cout << std::endl;
+    qs::Ket q00 = q0.tensor(q0);
+    print_qubit(q00);
 
     qs::Ket q10 = q1.tensor(q0);
-    q10.symbol();
-    std::cout << " = ";
-    q10.vector();
-    std::cout << std::endl;
+    print_qubit(q10);
+
+    qs::Ket q01 = q0.tensor(q1);
+    print_qubit(q01);
+
+    qs::Ket q11 = q1.tensor(q1);
+    print_qubit(q11);
+
+    qs::Ket q110 = q1.tensor(q1).tensor(q0);
+    print_qubit(q110);
 }
 
 void test_unitary() {
-    qs::Unitary h = qs::Hadamard();
-    h.symbol();
-    std::cout << " = " << std::endl;
-    h.matrix();
+    qs::Unitary H = qs::Hadamard();
+    print_unitary(H);
 
-    qs::Unitary h_dagger = h.dagger();
-    h_dagger.symbol();
-    std::cout << " = " << std::endl;
-    h_dagger.matrix();
+    qs::Unitary I = qs::Identity();
+    print_unitary(I);
 
-    qs::Unitary x = h + h_dagger;
-    x.symbol();
-    std::cout << " = " << std::endl;
-    x.matrix();
+    qs::Unitary X = qs::PauliX();
+    print_unitary(X);
 
-    x = h - h_dagger;
-    x.symbol();
-    std::cout << " = " << std::endl;
-    x.matrix();
+    qs::Unitary Y = qs::PauliY();
+    print_unitary(Y);
 
-    x = h * h_dagger;
-    x.symbol();
-    std::cout << " = " << std::endl;
-    x.matrix();
+    qs::Unitary Z = qs::PauliZ();
+    print_unitary(Z);
 
-    qs::Ket q0(qs::BasicQubits::ZERO);
-    qs::Ket q1 = h * q0;
-    q1.symbol();
-    std::cout << " = ";
-    q1.vector();
-    std::cout << std::endl;
+    qs::Unitary IX = I.tensor(X);
+    print_unitary(IX);
+
+    qs::Unitary XI = X.tensor(I);
+    print_unitary(XI);
+
+    qs::Unitary CNOT = qs::CNOT();
+    print_unitary(CNOT);
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
     std::cout << "Test Complex library" << std::endl;
     test_complex();
     std::cout << std::endl;
