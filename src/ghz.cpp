c9a0c8e64e723a6566f4a6623b0f783b1ff8eee7
Jakub Rada <dev.jakubrada@icloud.com>
2024-04-25 12:11:47 +0200

    Add ghz state simulation target

--- src/ghz.cpp
+++ src/ghz.cpp
@@ -1,24 +1,29 @@
 #include <iostream>
 
-#include "quantum/circuit.hpp"
-#include "quantum/unitary.hpp"
+#include "./quantum/circuit.hpp"
+#include "./quantum/unitary.hpp"
 
 int main(int argc, char* argv[]) {
-    qs::Circuit circuit(2, 2);
+    // initialize qubits to |0>
+    qs::Circuit circuit(3, 3, qs::BasicQubits::ZERO);
 
+    // apply Hadamard gate to qubit 0
     circuit.gate(qs::Hadamard(), 0);
+    // apply CNOT gate to qubit 1 with control qubit 0
+    circuit.cgate(qs::PauliX(), 0, 1);
+    // apply CNOT gate to qubit 2 with control qubit 1
+    circuit.cgate(qs::PauliX(), 1, 2);
 
+    // measure qubits into corresponding classical bits
     circuit.measure(0, 0);
     circuit.measure(1, 1);
+    circuit.measure(2, 2);
 
-    circuit.show();
-
+    // compile the circuit
     circuit.compile();
 
-    circuit.show();
-
+    // run the circuit
     qs::Results results = circuit.run(1000);
-
     results.show();
 
     return 0;
